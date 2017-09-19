92c4a4f891f56e1e486dc7ee218cfc882c47351a
Mario Alberto Avila Rehlaender <e-maavila@neoris.cxnetworks.net>
2017-09-19 16:05:49 -0500

    Generate routes for voice

---
+++ controllers/voiceformController.js
@@ -0,0 +1,15 @@
+exports.voice_form_post = function(req, res) {
+    res.send('This is a post');
+};
+
+exports.index = function(req, res) {
+    res.send('This is a get');
+};
+
+exports.post_voice = function(req, res) {
+    res.send('This is a post');
+};
+
+exports.get_voice = function(req, res) {
+    res.send('This is a get');
+};
--- routes/voiceform.js
+++ routes/voiceform.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('voiceform', {title: 'respond with a resource'});
-});
+
+var voiceform_controller = require('../controllers/voiceformController');
+
+// form page
+router.get('/', voiceform_controller.index);
+
+/* POST request speaking. */
+router.post('/create', voiceform_controller.post_voice);
+
+/* GET request for speaking. */
+router.get('/get', voiceform_controller.get_voice);
+
 
 module.exports = router;
